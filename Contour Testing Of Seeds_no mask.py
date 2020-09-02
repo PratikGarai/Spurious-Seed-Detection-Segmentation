5bd604cf0d23872420d7327cba3b45518085229f
PratikGarai <pratikgarai0208@gmail.com>
2020-09-02 16:33:32 +0530

    cleaned the scripts

--- Contour Testing Of Seeds_no mask.py
+++ Contour Testing Of Seeds_no mask.py
@@ -1,16 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import cv2 as cv
 import numpy
 import random   # for testing purpose only
-
-
-# In[2]:
-
 
 def process_contour(contour):
     x,y,w,h = cv.boundingRect(contour)
@@ -20,20 +10,12 @@ def process_contour(contour):
     result = random.randint(0,1)
     return result
 
-
-# In[3]:
-
-
 def filter_contours(contours, hierarchy):
     c = []
     for i,j in zip(contours,hierarchy[0]):
         if(j[3]==-1):
             c.append(i)
     return c
-
-
-# In[4]:
-
 
 def get_contoured_image(im):
     imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
@@ -50,19 +32,11 @@ def get_contoured_image(im):
         cv.drawContours(im, contours, index, color, 2)
     return im
 
-
-# In[5]:
-
-
 def segment_printer(segments):
     for index,i in enumerate(segments):
         cv.imshow(str(index+1), cv.resize(i, (200,int(200/i.shape[1]*i.shape[0]))))
         cv.waitKey()
         cv.destroyAllWindows()
-
-
-# In[6]:
-
 
 img = cv.imread('images/img2.jpg')
 segments = []     #for testing purpose only     
@@ -73,10 +47,3 @@ cv.imshow("Output", new_image)
 cv.waitKey()
 cv.destroyAllWindows()
 segment_printer(segments)    # for testing only
-
-
-# In[ ]:
-
-
-
-
